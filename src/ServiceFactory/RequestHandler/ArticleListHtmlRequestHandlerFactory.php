53d49a30f795d0695e674444a6cc3956b0e9e29d
evan70 <ivanpaldan@yandex.com>
2025-10-10 22:33:43 +0200

    Fix ArticleListHtmlRequestHandlerFactory to pass both template and repository to constructor

--- src/ServiceFactory/RequestHandler/ArticleListHtmlRequestHandlerFactory.php
+++ src/ServiceFactory/RequestHandler/ArticleListHtmlRequestHandlerFactory.php
@@ -13,6 +13,7 @@ final class ArticleListHtmlRequestHandlerFactory
     public function __invoke(ContainerInterface $container): ArticleListHtmlRequestHandler
     {
         return new ArticleListHtmlRequestHandler(
+            $container->get(\Mezzio\Template\TemplateRendererInterface::class),
             $container->get(ArticleRepository::class)
         );
     }
